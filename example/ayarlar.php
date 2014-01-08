0a7d6f613839f1f721c16e27e3ce62761b4bd824
Tayfun Erbilen <tayfunerbilen@gmail.com>
2014-01-08 03:05:55 +0200

    Create ayarlar.php

---
+++ example/ayarlar.php
@@ -0,0 +1,10 @@
+<?php
+$ayarlar['adsoyad'] = "Mert Osman Başol";
+$ayarlar['eposta'] = "tayfun@erbilen.net";
+$ayarlar['telefon'] = "0541 749 57 44";
+$ayarlar['web'] = "http://www.erbilen.net";
+$ayarlar['hakkinda'] = "ben bir garip keloğlanım :)";
+$ayarlar['facebook'] = "http://www.facebook.com/tayfunerbilen";
+$ayarlar['twitter'] = "http://www.twitter.com/tayfunerbilen";
+$ayarlar['youtube'] = "http://www.youtube.com/tayfunerbilen";
+?>
